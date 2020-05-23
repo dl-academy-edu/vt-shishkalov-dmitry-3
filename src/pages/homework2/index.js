6bb6ccaf54ebf465d6173ebaed4c911d956d2dbc
Dmitrii64 <60173944+Dmitrii64@users.noreply.github.com>
2020-05-23 20:43:36 +0400

    Исправил

--- src/pages/homework2/index.js
+++ src/pages/homework2/index.js
@@ -1,35 +1,35 @@
 let i = 1;
 
-function f() {
+function task1() {
   let max = +prompt("Введите число (Не более 40)", 7);
   if (Number.isNaN(max)) {
     alert("Необходимо ввести число!");
-    f()
+    task1()
   }
   if (max > 40) {
     alert("Слишком большое число!");
     max = 0;
-    f()
+    task1()
   }
   for (i; i <= max; i++) {
-    if (i % 4 == 0) {
+    if (i % 4) {
+      console.log(i);
       continue
     }
-    console.log(i);
   }
 }
 
-function f2() {
+function task2() {
   console.log(num);
   num = num + 3;
 }
 
 console.log("Результат первой программы:");
-f()
+task1()
 console.log("Результат второй программы:");
 let num = +prompt("Введите другое число", 9);
 let timerId = setInterval(function () {
-  f2();
+  task2();
 }, 3000);
 setTimeout(function () {
   clearInterval(timerId);
